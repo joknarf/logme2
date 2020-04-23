6636cb2648c6494f537cb45f38e2fe26908a5770
joknarf <joknarf@free.fr>
2020-04-24 00:44:32 +0200

    [logme] add python_wrapper

---
+++ server2.py
@@ -0,0 +1,15 @@
+#!./python_wrapper
+ 
+import BaseHTTPServer
+import CGIHTTPServer
+ 
+PORT = 8888
+server_address = ("", PORT)
+
+server = BaseHTTPServer.HTTPServer
+handler = CGIHTTPServer.CGIHTTPRequestHandler
+handler.cgi_directories = ["/"]
+print "Serveur actif sur le port :", PORT
+
+httpd = server(server_address, handler)
+httpd.serve_forever()
